001c15070bb7cedbaef6453230454bd43f21d04f
Dannansilva <dsssgunathilake@students.ac.lk>
2023-08-21 13:30:51 +0530

    added pic to the home page

--- crashcourse/lib/page/home_page.dart
+++ crashcourse/lib/page/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:crashcourse/styles/app_colors.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -6,7 +7,41 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      appBar: AppBar(
+        backgroundColor: AppColors.background,
+        title: Text('5min flutter'),
+        centerTitle: false,
+        actions: [
+          Icon(Icons.location_off_outlined),
+        ],
+      ),
+      body: Column(
+        children: mockUsersFromServer(),
+      ),
     );
+  }
+
+  Widget _userItem() {
+    return Row(
+      children: [
+        Image.asset(
+          'assets/temp/dannan.png',
+          width: 100,
+          height: 100,
+        ),
+        SizedBox(
+          width: 16,
+        ),
+        Text('Dannan Silva'),
+      ],
+    );
+  }
+
+  List<Widget> mockUsersFromServer() {
+    List<Widget> users = [];
+    for (var i = 0; i < 20; i++) {
+      users.add(_userItem());
+    }
+    return users;
   }
 }
--- crashcourse/lib/page/login_page.dart
+++ crashcourse/lib/page/login_page.dart
@@ -80,7 +80,7 @@ class Loginpage extends StatelessWidget {
                   height: 50,
                   child: ElevatedButton(
                     onPressed: () {
-                      Navigator.of(context).pushNamed('/home');
+                      Navigator.of(context).pushReplacementNamed('/home');
                     },
                     style: ElevatedButton.styleFrom(
                       backgroundColor: Color.fromARGB(255, 134, 163, 207),
